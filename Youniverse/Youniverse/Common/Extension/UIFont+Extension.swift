7dc1150b54f314bcff3767c89ecd70e50282f3b7
Danna <daeunlee0720@gmail.com>
2022-03-06 17:48:18 +0900

    feat: 행성 이름 추가

---
+++ Youniverse/Youniverse/Common/Extension/UIFont+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIFont+Extension.swift
+//  Youniverse
+//
+//  Created by Danna Lee on 2022/03/06.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    class func JejuMyeongjo(size: CGFloat) -> UIFont {
+        return UIFont(name: "JejuMyeongjoOTF", size: size)!
+    }
+}
--- Youniverse/Youniverse/Scene/Main/ViewController.swift
+++ Youniverse/Youniverse/Scene/Main/ViewController.swift
@@ -9,13 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - IBOutlet
     @IBOutlet var logoView: TopLogoView? = TopLogoView()
     
+    @IBOutlet var planetNameLabel: UILabel!
+    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setStyle()
     }
 
 
+}
+
+extension ViewController {
+    func setStyle() {
+        planetNameLabel.textAlignment = .center
+        planetNameLabel.font = .JejuMyeongjo(size: 16)
+        planetNameLabel.text = "고양이냥"
+        planetNameLabel.textColor = .white
+    }
 }
 
